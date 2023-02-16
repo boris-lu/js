b52b75ee79c2fffa9be227b559d185884a69b815
Boris L <Boris.Lukashenko@gmail.com>
2023-02-16 16:13:57 +0200

    23.2 Цикл с метками выхода

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ const leght = 7;
 
 for (let i = 1; i < leght; i++) {
   for (let j = 0; j < i; j++) {
-    result += "*";
+    result += "*"; // присваиваем текстовое значение к результату
   }
 
   result += "\n";
 }
 
 console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+  console.log(`First lelel: ${i}`);
+  for (let j = 0; j < 3; j++) {
+    console.log(`Second lelel: ${j}`);
+    for (let k = 0; k < 3; k++) {
+      if (k === 2) continue first; // выход из цикла, если поставить break - будет полная остановка
+      console.log(`Third lelel: ${k}`);
+    }
+  }
+}
